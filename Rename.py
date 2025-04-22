d531841fccd4eadaa1e01de9a3a16fbfa2d6bacd
KemboiM <millsortrobins@gmail.com>
2025-04-22 17:55:11 +0300

    Update Rename.py

--- Rename.py
+++ Rename.py
@@ -1,8 +1,8 @@
 import os
 
-folder = r"C:\Users\Milsort\Dropbox\computer_vision\semantic_segmentation\data\wheat\all_training_data\tiled_data\fltr"
+folder = r"C:\Users\path"
 
 for filename in os.listdir(folder):
-    if "_watershed_mask" in filename:
-        new_name = filename.replace("_watershed_mask", "_mask")
+    if "old_name" in filename:
+        new_name = filename.replace("old_Name", "current_name")
         os.rename(os.path.join(folder, filename), os.path.join(folder, new_name))
